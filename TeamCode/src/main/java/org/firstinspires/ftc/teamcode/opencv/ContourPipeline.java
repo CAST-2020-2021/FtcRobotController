495536905165d335d3c3d805e1f799cdb175490a
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2021-06-18 14:18:02 +0100

    contour

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/ContourPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/ContourPipeline.java
@@ -1,19 +1,33 @@
 package org.firstinspires.ftc.teamcode.opencv;
 
-import android.os.strictmode.ImplicitDirectBootViolation;
-
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContourPipeline extends OpenCvPipeline
 {
     // Notice this is declared as an instance variable (and re-used), not a local variable
     Mat grey = new Mat();
+    Mat limited = new Mat();
+
 
     @Override
     public Mat processFrame(Mat input)
     {
+        Imgproc.cvtColor(input,grey, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.threshold(grey, limited,127,255, Imgproc.ADAPTIVE_THRESH_MEAN_C);
+
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(limited, contours, hierarchy, Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.drawContours(input, contours,-1 ,new Scalar(255,255,255),3);
+        hierarchy.release();
         return input;
+
     }
 }
